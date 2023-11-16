c73f9ea241c2f136bf0defdea186d6beed1e3acc
aayush2870 <aayushraj2881@gmail.com>
2023-11-17 01:10:28 +0530

    added hover to cards
    added hover to cards

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 var crsr = document.querySelector("#cursor");
 
 document.addEventListener("mousemove", function (dets) {
-  crsr.style.left = dets.x + "px";
+  crsr.style.left = dets.x +30+ "px";
   crsr.style.top = dets.y + "px";
+});
+
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    crsr.style.scale = 3;
+    crsr.style.border = "1px solid #fff";
+    crsr.style.backgroundColor = "transparent";
+  });
+  elem.addEventListener("mouseleave", function () {
+    crsr.style.scale = 1;
+    crsr.style.border = "0px solid #95C11E";
+    crsr.style.backgroundColor = "#95C11E";
+  });
 });
 
 gsap.to("#nav", {
@@ -29,5 +43,19 @@ gsap.to("#main", {
     start: "top -25%",
     end: "top -70%",
     scrub: 2,
+  },
+});
+
+gsap.from("#about-me img,#about-me-in", {
+  y: 90,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#about-me",
+    scroller: "body",
+    // markers:true,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
   },
 });
